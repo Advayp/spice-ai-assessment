dd56f9174f43a48e7ca9d656dd70a326ce7d80b4
Advay Patil <advaypatil27@gmail.com>
2024-10-22 16:56:51 -0700

    Fixes #9: Create custom catcher for 404 errors

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@ extern crate rocket;
 extern crate dotenv;
 
 mod database;
+mod errors;
 mod models;
 mod routes;
 mod types;
 
 use dotenv::dotenv;
+use errors::not_found;
 use routes::scores::{add_scores, fetch_all_scores};
 
 // Create test route to ensure API is functional
@@ -20,5 +22,7 @@ pub fn test() -> &'static str {
 pub fn rocket() -> _ {
     dotenv().ok();
 
-    rocket::build().mount("/api", routes![test, fetch_all_scores, add_scores])
+    rocket::build()
+        .mount("/api", routes![test, fetch_all_scores, add_scores])
+        .register("/", catchers![not_found])
 }
