7b1c8dc52d8445ff1d979332aa805b7963bf07cb
Advay Patil <advaypatil27@gmail.com>
2024-10-22 16:40:29 -0700

    Fixes #7: Fix Required Fields Bug

--- src/routes/scores.rs
+++ src/routes/scores.rs
@@ -23,15 +23,35 @@ pub async fn fetch_all_scores() -> Json<ScoreResponse> {
 }
 
 #[derive(Deserialize, Debug, Clone)]
+struct ScoreFields {
+    name: String,
+    uid: String,
+    score: u64,
+    notes: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
 pub struct AddScoreRequest {
-    scores: Vec<ScoreInfo>,
+    scores: Vec<ScoreFields>,
 }
 
 #[post("/scores/insert", data = "<scores>")]
 pub async fn add_scores(scores: Json<AddScoreRequest>) -> Json<ValidateResponse> {
     let all_scores = scores.into_inner().scores;
 
-    insert_rows(&all_scores).await.unwrap();
+    let mut converted_scores: Vec<ScoreInfo> = vec![];
+
+    for score in all_scores {
+        converted_scores.push(ScoreInfo {
+            name: score.name.clone(),
+            uid: score.uid.clone(),
+            score: score.score.clone(),
+            notes: score.notes.clone(),
+            ..Default::default()
+        });
+    }
+
+    insert_rows(&converted_scores).await.unwrap();
 
     Json(ValidateResponse { success: true })
 }
