33aa8212e0c65274741019bbf92a9803ad53970f
Advay Patil <advaypatil27@gmail.com>
2024-10-22 18:34:41 -0700

    #13: Add helper functions for auth

---
+++ src/database/user.rs
@@ -0,0 +1,39 @@
+use reqwest::Response;
+use rocket::serde::{Deserialize, Serialize};
+
+use super::utils::{build_auth_url, build_request_client, RequestTypes};
+
+#[derive(Serialize, Deserialize)]
+pub struct AuthData {
+    email: String,
+    password: String,
+}
+
+pub async fn sign_in(
+    email: String,
+    password: String,
+) -> Result<Response, Box<dyn std::error::Error>> {
+    let url = build_auth_url("signup".to_string());
+
+    let req = build_request_client(&url, RequestTypes::POST);
+
+    let data = AuthData { email, password };
+
+    let res = req.json(&data).send().await?;
+
+    Ok(res)
+}
+
+pub async fn login(
+    email: String,
+    password: String,
+) -> Result<Response, Box<dyn std::error::Error>> {
+    let url = build_auth_url("/token?grant_type=password".to_string());
+    let req = build_request_client(&url, RequestTypes::POST);
+
+    let data = AuthData { email, password };
+
+    let res = req.json(&data).send().await?;
+
+    Ok(res)
+}
